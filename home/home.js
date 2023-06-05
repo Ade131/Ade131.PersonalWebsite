4b8bc18cfa19dad7c858e981b0b164cd0104cfb3
Ade131 <aidankelly202@gmail.com>
2023-06-05 20:40:24 +0100

    Adjusted nav bar functionality

--- home/home.js
+++ home/home.js
@@ -1,13 +1,18 @@
 var prevScrollPos = window.pageYOffset;
+var header = document.querySelector("header");
 
 window.onscroll = function() {
     var currentScrollPos = window.pageYOffset;
+
     if (prevScrollPos > currentScrollPos) {
         //Scrolling up
-        document.querySelector("header").classList.remove("hide");
-    } else {
+        if (currentScrollPos < 600) {
+            //If user is near the top of the page
+            header.classList.remove("hide");
+        }
+    } else if (currentScrollPos > 300) {
         //Scrolling down
-        document.querySelector("header").classList.add("hide");
+        header.classList.add("hide");
     }
     prevScrollPos = currentScrollPos;
     };
