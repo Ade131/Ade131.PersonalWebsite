8c1d1a88a57f8b1b0b7fab6e2e9ab64b0ff52b5c
Ade131 <aidankelly202@gmail.com>
2023-06-05 22:03:59 +0100

    Added header shadow

--- home/home.js
+++ home/home.js
@@ -1,14 +1,25 @@
+window.onbeforeunload = function() {
+    window.scrollTo(0, 0);
+};
+
 var prevScrollPos = window.pageYOffset;
 var header = document.querySelector("header");
 
 window.onscroll = function() {
     var currentScrollPos = window.pageYOffset;
 
+    if (currentScrollPos < 100) {
+        //At the top of page
+        header.classList.remove("shadow");
+        header.classList.remove("hide");
+    }
+
     if (prevScrollPos > currentScrollPos) {
         //Scrolling up
-            header.classList.remove("hide");
+        header.classList.remove("hide");
     } else if (currentScrollPos > 200) {
         //Scrolling down
+        header.classList.add("shadow")
         header.classList.add("hide");
     }
     prevScrollPos = currentScrollPos;
