fefff8cafea40433e6018a9910adc9ea155634fb
Ade131 <aidankelly202@gmail.com>
2023-06-06 10:22:58 +0100

    Added mobile view hamburger menu functionality

--- home/home.js
+++ home/home.js
@@ -1,17 +1,17 @@
+//Set position to top when reloading
 window.onbeforeunload = function() {
     window.scrollTo(0, 0);
 };
 
-//Header hide/show
+//Header hide/show when scrolling
 var prevScrollPos = window.pageYOffset;
-var header = document.querySelector("header");
-var scrollUpThreshold = 10;
+const header = document.querySelector(".header");
 
 window.onscroll = function() {
     var currentScrollPos = window.pageYOffset;
 
     if (currentScrollPos < 100) {
-        //At the top of page
+        //At the top of the page
         header.classList.remove("shadow");
         header.classList.remove("hide");
     }
@@ -27,4 +27,52 @@ window.onscroll = function() {
     prevScrollPos = currentScrollPos;
     };
 
+//Hamburger menu logic
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const content = document.querySelector(".content");
+const body = document.querySelector("body");
+
+hamburger.addEventListener("click", mobileMenu);
+body.addEventListener("click", closeMenuOutside);
+
+//When menu is opened
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+    content.classList.toggle("blur");
+    //Disable scrolling when menu is open
+    if (content.classList.contains("blur")) {
+        body.style.overflow = "hidden";
+    } else {
+        body.style.overflow = "";
+    }
+}
+
+//Close menu when user clicks outside the menu box
+function closeMenuOutside(event) {
+    if (
+        content.classList.contains("blur") &&
+        !navMenu.contains(event.target) &&
+        !hamburger.contains(event.target)
+    ) {
+        //Click occurred outside the menu box
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+        content.classList.remove("blur");
+        body.style.overflow = "";
+    }
+}
+
+//Close menu when option selected
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+    content.classList.remove("blur");
+    body.style.overflow = "";
+}
   
